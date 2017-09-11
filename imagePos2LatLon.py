71cda85368bbb469f8f6bd561f75416015eae160
Henrik <hes1990@gmail.com>
2017-09-11 15:00:15 +0200

    restructured code to improve performance.

--- imagePos2LatLon.py
+++ imagePos2LatLon.py
@@ -12,6 +12,10 @@ class ReconstructionError(Exception):
     pass
 
 
+class NoImageError(Exception):
+    pass
+
+
 class ImagePointToLatLon:
     def __init__(self, project):
         self.project = project
@@ -25,6 +29,10 @@ class ImagePointToLatLon:
         self.gps_namedtuple = namedtuple('gps', ['lat', 'lon'])
         self.geo_model = self.get_geo_model()
         self.geo_transform = self.get_geo_transform()
+        self.image = None
+        self.image_name = None
+        self.depth_map = None
+        self.recon = None
 
     def get_geo_transform(self):
         geo_file = self.project \
@@ -55,17 +63,17 @@ class ImagePointToLatLon:
                                               north_offset)
         return geo_model
 
-    def get_reconstruction(self, image_name, image):
+    def get_reconstruction(self):
         k = None
         q = None
         origin = None
         reconstruction_file = self.project + '/opensfm/reconstruction.nvm'
-        row_matcher = re.compile(image_name + (' ' + self.regex_f) * 8)
+        row_matcher = re.compile(self.image_name + (' ' + self.regex_f) * 8)
         with open(reconstruction_file, 'r') as f:
             for line in f:
                 match = re.search(row_matcher, line)
                 if match:
-                    k = self.get_k(float(match.group(1)), image)
+                    k = self.get_k(float(match.group(1)), self.image)
                     q = Quaternion(float(match.group(2)), float(match.group(3)),
                                    float(match.group(4)), float(match.group(5)))
                     origin = np.array([float(match.group(6)),
@@ -73,9 +81,10 @@ class ImagePointToLatLon:
                                        float(match.group(8))])
         if k is None:
             raise ReconstructionError('Image \'%s\' is not part of the '
-                                      'reconstruction.nvm file.' % image_name)
+                                      'reconstruction.nvm file.' %
+                                      self.image_name)
         reconstruction = self.reconstruction_namedtuple(k, q, origin)
-        return reconstruction
+        self.recon = reconstruction
 
     @staticmethod
     def get_k(focal, image):
@@ -87,25 +96,27 @@ class ImagePointToLatLon:
         k[1, 2] = image.shape[0] / 2
         return k
 
-    def get_depth(self, image_name, image, x, y):
-        depth_map_file = self.project + '/opensfm/depthmaps/' + image_name \
-                         + '.clean.npz'
+    def get_depth(self, image, x, y):
+        scale = self.depth_map.shape[1] / image.shape[1]
+        x_new = round(scale * x)
+        y_new = round(scale * y)
+        depth = self.depth_map[y_new, x_new]
+        if depth == 0:
+            depth = self.find_nearest_nonzero(self.depth_map, x_new, y_new)
+            print('Warning: pixel (%i, %i) not part of depth model. '
+                  'Nearest pixel in depth model is used, which may give some '
+                  'offset in the position.' % (x, y))
+        return depth
+
+    def get_depth_map(self):
+        depth_map_file = self.project + '/opensfm/depthmaps/' \
+                         + self.image_name + '.clean.npz'
         try:
             depth_map = np.load(depth_map_file)['depth']
         except FileNotFoundError:
             print('Image is not part of the reconstruction')
             raise
-        cv2.imwrite('test.png', depth_map)
-        scale = depth_map.shape[1] / image.shape[1]
-        x_new = round(scale * x)
-        y_new = round(scale * y)
-        depth = depth_map[y_new, x_new]
-        if depth == 0:
-            depth = self.find_nearest_nonzero(depth_map, x_new, y_new)
-            print('Warning: pixel (%i, %i) not part of depth model. '
-                  'Nearest pixel in depth model is used, which may give some '
-                  'offset in the position.' % (x, y))
-        return depth
+        self.depth_map = depth_map
 
     @staticmethod
     def find_nearest_nonzero(depth_map, x, y):
@@ -129,7 +140,7 @@ class ImagePointToLatLon:
         return geo_p
 
     def utm_to_lat_lon(self, p):
-        hemisphere, zone, e_offset, n_offset = self.get_geo_model()
+        hemisphere, zone, e_offset, n_offset = self.geo_model
         east = p[0] + e_offset
         north = p[1] + n_offset
         utm_converter = utmconv()
@@ -137,7 +148,7 @@ class ImagePointToLatLon:
         pos = self.gps_namedtuple(lat, lon)
         return pos
 
-    def get_lat_lon(self, image_name, x, y):
+    def open_image(self, image_name):
         image_file = self.project + '/images/' + image_name
         image = cv2.imread(image_file)
         if image is None:
@@ -145,16 +156,38 @@ class ImagePointToLatLon:
             raise FileNotFoundError(errno.ENOENT,
                                     os.strerror(errno.ENOENT),
                                     image_file)
-        depth = self.get_depth(image_name, image, x, y)
-        recon = self.get_reconstruction(image_name, image)
-        point3d = self.get_3d_point(x, y, depth, recon)
+        self.image = image
+        self.image_name = image_name
+        self.get_depth_map()
+        self.get_reconstruction()
+
+    @staticmethod
+    def check_input_coordinates(image, x, y):
+        if image.shape[1] < x:
+            raise ValueError('x of %i is outside the image' % x)
+        elif x < 0:
+            raise ValueError('x must be positive, not %i' % x)
+        if image.shape[0] < y:
+            raise ValueError('y of %i is outside the image' % y)
+        elif y < 0:
+            raise ValueError('y must be positive, not %i' % y)
+
+    def get_lat_lon(self, x, y):
+        if self.image is None:
+            raise NoImageError('Image has not been set.')
+        self.check_input_coordinates(self.image, x, y)
+        depth = self.get_depth(self.image, x, y)
+        point3d = self.get_3d_point(x, y, depth, self.recon)
         utm_point = self.get_geo_3d_point(point3d)
         pos = self.utm_to_lat_lon(utm_point)
         return pos
 
 
 if __name__ == '__main__':
-    image_pos = ImagePointToLatLon('/home/henrik/droneMap/projects/hojby')
-    gps_pos = image_pos.get_lat_lon('DJI_0361.JPG', 5000, 1)
+    folder = '/home/henrik/mount/henrikServer/Documents/openDroneMap/projects/'
+    folder += '2017-09-06-Pumpkins-50m-field1'
+    image_pos = ImagePointToLatLon(folder)
+    image_pos.open_image('DJI_0056.JPG')
+    gps_pos = image_pos.get_lat_lon(4584, 1044)
     print(gps_pos.lat)
     print(gps_pos.lon)
